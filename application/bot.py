96ee91f06ab90995f55de5165af4785073b06de8
xqweak <xqweak@gmail.com>
2023-09-19 00:01:03 -0500

    gonna start the bot in a non confusing way

--- application/bot.py
+++ application/bot.py
@@ -5,31 +5,10 @@ from handlers import HostList
 from discord.ext import commands
 import discord
 
-import discord
-from discord.ext import commands
-
-class Scoop(commands.Bot):
-    def __init__(self, command_prefix):
-        intents = discord.Intents.default()
-        intents.typing = False
-        super().__init__(command_prefix=command_prefix, intents=intents)
-
-    async def on_ready(self):
-        print(f'Logged in as {self.user.name}')
-
-    @commands.command()
-    async def options(self, ctx):
-        # Create a message with clickable options
-        print("hola")
 
 
-
-def main():
-    with open('token.txt' , 'r') as fp:
-        TOKEN = fp.read()
-    bot = Scoop(command_prefix='!')
-    bot.run(TOKEN)
-
+# Main function
 if __name__ == "__main__":
-    main()
-
+    with open('token.txt', 'r') as fp:
+        TOKEN = fp.read()
+    bot.run(TOKEN)
